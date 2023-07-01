f8d0c693ee87048ac143a100cde5f5643ce20a2c
Ashish <ashishravidas2204@gmail.com>
2023-07-01 07:50:07 +0530

    fixed bugs

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -6,7 +6,7 @@ dotenv.config();
 const router = express.Router();
 
 const configuration = new Configuration({
-    apiKey: "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",
+    apiKey: process.env.OPENAI_API_KEY,
   });
 const openai = new OpenAIApi(configuration);
 
